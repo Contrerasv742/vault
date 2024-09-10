04e3b9a20810ee921e8b009abb62c33fa71a0339
contrerasv742 <contrerasv742@gmail.com>
2024-09-09 19:03:39 -0700

    Merging Caesar branch (main work) to Main

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Purpose
-Intend to make a password manager that will allow me to learn common encryption methods such as RSA and develop an app to store my passwords locally.
+Intended to create a password manager with RSA Encryption to properly store my personal passwords locally. Project will also aid in understanding core encryption algorithms. Additionally, I will learn about making cross platform applications.
+
+# Compilation
+```bash
+    mkdir -p build
+    cd build
+    cmake ..
+    make
+```
+
+# Execution
+```bash
+    ./encryption
+```
+
+# Dates
+Start: July 2024
+End: TBD
+
+# Contributors
+Victor Perez Contreras
---
+++ src/mainpage.h
@@ -0,0 +1,52 @@
+#ifndef MAINPAGE_H
+#define MAINPAGE_H
+
+#include "encryptpage.h"
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QScrollArea>
+
+class MainPage : public QWidget
+{
+  Q_OBJECT
+
+  public:
+  explicit MainPage(QWidget *parent = nullptr);
+
+private slots:
+  void toggleOptions();
+  void toggleFavorite(const QString &companyName);
+  void showEncryptView();
+  void showPasswordView();
+
+private:
+  void setupLeftContainer();
+  void setupRightContainer();
+  void createPasswordCard(const QString &companyName, const QString &username);
+  void reorderCards(QGridLayout *layout);
+  QPushButton* createStyledButton(const QString &text, const QString &iconName, bool showText = true);
+  void filterPasswords(const QString &searchText);
+  void setupEncryptPage();
+  void setupPasswordView();
+  void setupEncryptView();
+
+  QWidget *leftContainer;
+  QWidget *rightContainer;
+  QLineEdit *searchBar;
+  QPushButton *optionsButton;
+  QWidget *optionsWidget;
+  QScrollArea *favoritesScrollArea;
+  QScrollArea *allPasswordsScrollArea;
+  EncryptPage *encryptPage;
+  QWidget *passwordView;
+  QWidget *encryptView;
+
+  int favoritesCardCount;
+  int allPasswordsCardCount;
+};
+
+#endif // MAINPAGE_H
