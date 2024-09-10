88ce4f055fa0fffd845d33622c7df58406c453f4
contrerasv742 <contrerasv742@gmail.com>
2024-09-09 18:56:26 -0700

    Beginning Parser

--- src/parser.h
+++ src/parser.h
@@ -1,17 +1,33 @@
 #ifndef PARSER_H
-#include <iostream>
 #include <fstream>
+#include <vector>
+
+class Password {
+private: 
+  std::string company_;
+  std::string username_;
+  std::string password_;
+  bool favorite_;
+
+public: 
+  Password(std::string company, std::string username, std::string password): company_(company), username_(username), password_(password), favorite_(false){};
+};
 
 class Parser {
-
 private:
   std::string filename_;
   std::ifstream file_;
+  std::vector<Password> passwords_;
   
 public:
   Parser(std::string filename);
-  void view();
+  std::string view();
+  int find(std::string item);
+  int add();
+  int remove();
+  int readFile();
 
+  ~Parser() { file_.close();}
 };
 
 #endif // PARSER_H
